5caf1906f2306d3bf68279fd6b4b4cf373ee6396
UktamovaSabina <sabinauktamova12052002@gmail.com>
2023-08-29 23:15:30 +0500

    removed unused codes

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import * as express from 'express';
-import { resolve } from 'path';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use('/uploads', express.static(resolve('uploads')))
   app.enableCors({ origin: "*" })
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }))
   const config = new DocumentBuilder()
